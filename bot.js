0b83cbae8f6e0459c57230b95f7fb93878f6d759
Jenish Mangukiya <34957550+jenishmangukiya@users.noreply.github.com>
2019-07-20 04:19:03 -0400

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+const https = require('https')
+
+client.on('ready', () => {
+  console.log('Working!');
+});
+
+client.on('guildMemberAdd', member => {
+  const channel = member.guild.channels.find(ch => ch.name === 'general-chat');
+  const msgchannel = member.guild.channels.find(ch => ch.name === 'tradingview-id').toString();
+  const faqchannel = member.guild.channels.find(ch => ch.name === 'apex-faq').toString();
+  if (!channel) return;
+  channel.send(`:confetti_ball:  Welcome to the server : ${member}. You can drop your Tradingview :id:/Username in ${msgchannel} and don't forgot to read ${faqchannel}.`);
+});
+
+client.login(process.env.BOT_TOKEN);
