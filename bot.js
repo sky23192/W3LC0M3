fe6b3830be6f57d2cfb15610ca2d3625e3d5784b
sky23192 <sky23192@gmail.com>
2019-07-31 16:44:08 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,9 +10,15 @@ client.on('guildMemberAdd', member => {
   const channel = member.guild.channels.find(ch => ch.name === 'general-chat');
   const msgchannel = member.guild.channels.find(ch => ch.name === 'tradingview-id').toString();
   const faqchannel = member.guild.channels.find(ch => ch.name === 'apex-faq').toString();
+  const signalch = member.guild.channels.find(ch => ch.name === 'apex-signals').toString();
+  
   if (!channel) return;
-  channel.send(`:confetti_ball:  Welcome to the server ${member}. You can drop your Tradingview profile name in ${msgchannel} to get access of ape[x] and don't forgot to read ${faqchannel}.`);
+  channel.send(`:confetti_ball:  Welcome to the server ${member}. You can drop your Tradingview profile name in ${msgchannel} to get access of ape[x]. Please note the public trial period which had started on 1st of june expired on 31st july. Please wait till it re-opens. Till then you can check out ${signalch} & ${faqchannel}.`);
   console.log('Added');
 });
+
+:confetti_ball:  Welcome to the server ID. You can drop your Tradingview profile name in #tradingview-id to get access of ape[x],
+Please note the public trial period which had started on 1st of june expired on 31st july.
+Please wait till it reopens. Till then you can check out #apex-signals & #apex-faq
 
 client.login(process.env.BOT_TOKEN);
